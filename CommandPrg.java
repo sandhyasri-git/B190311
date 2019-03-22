c4321eb8e2e5be24a626b6be1df5eb2530ae402b
sandhyasri-git <sandhyasri-git@users.noreply.github.com>
2019-03-22 10:45:14 +0530

    Add files via upload

---
+++ CommandPrg.java
@@ -0,0 +1,17 @@
+public class CommandPrg {
+
+	public static void main(String[] args) {
+		
+		if(args.length==2)
+		{
+			System.out.println(args[0]);
+			System.out.println(args[1]);
+		}
+		else
+		{
+			System.out.println("Invalid Number of Arguments");
+		}
+
+	}
+
+}
